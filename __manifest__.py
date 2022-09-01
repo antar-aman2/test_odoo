148f766c46fda6970a14a46da5f686a10848389e
antar-aman2 <antar2009500@gmail.com>
2022-09-01 12:44:40 +0530

    [add] rhis my new files

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Hospital Test',
+    'version': '1.0',
+    'license': 'LGPL-3',
+    'category': 'Heals Care',
+    # 'depends': [
+    #     ''
+    # ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/hospital_pation.xml',
+        
+    ],
+    
+    
+    'auto_install': False,
+    'application': False,
+    
+}
